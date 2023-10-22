c56b08aca8538824bb66985e2817f09f0277bac8
nursultanxabipnazarov <nursultanxabipnazarov@gmail.com>
2023-10-22 21:42:58 +0500

    Users full created function form

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -2,27 +2,54 @@
 
 include __DIR__.'   /../fun.php';
 
- if(isset($_POST['reg'])){
+
+if($_SERVER['REQUEST_METHOD']==="POST" && isset($_POST['reg'])){
+    $erMsg = [];
+        $admin = 0;
+        $fname = trim($_POST['fname']);
+        $lname = trim($_POST['lname']);
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+        $password2 = trim($_POST['password2']);
+
+        if($fname === ''  || $lname === '' || $email === '' || $password === '' ){
+
+           array_push($erMsg,"Не все поля заполнены!");
+        }elseif(mb_strlen($email,'UTF8')<2){
+            array_push($erMsg,"email должен быть более 2-х символов");
+        }elseif($password !== $password2){
+            array_push($erMsg,"parol qate !");
+        }else{
+            $existence = selectOne('users',['email'=>$email]);
+            if($existence['email']===$email){
+                array_push($erMsg,"Пользователь с такой почтой уже зарегистрирован!");
+            }else{
+                $pass = password_hash($password,PASSWORD_DEFAULT);
+                $user = [
+                    'admin'=>$admin,
+                     'fname'=>$fname,
+                     'lname'=>$lname,
+                     'email'=>$email,
+                     'password'=>$pass,
+            
+                ];
+
+                $id = insert('users',$user);
+                test($id);
+            }
+        }
+
+
+       
+      
+       
+}else
+
+{
+
+    echo "GET";
+}
+
  
-     $fname = $_POST['fname'];
-     $lname = $_POST['lname'];
-     $email = $_POST['email'];
-     $password = password_hash($_POST['password'] ,PASSWORD_DEFAULT);
-     $admin = 0;
-
-    $user = [
-        'admin'=>$admin,
-         'fname'=>$fname,
-         'lname'=>$lname,
-         'email'=>$email,
-         'password'=>$password
-
-    ];
- 
-  test($user);
-
-        $users = insert('users' , $user);
-        test($users);
- }
 
 ?>
--- app/fun.php
+++ app/fun.php
@@ -75,6 +75,34 @@ require __DIR__.'/database/conn.php';
 }
 
 
+function selectOne($table,$param = []){
+    global $conn;
+    $sql = "SELECT * FROM $table";
+
+    if(!empty($params)){
+        $i = 0;
+        foreach ($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'".$value."'";
+            }
+            if ($i === 0){
+                $sql = $sql . " WHERE $key=$value";
+            
+            }else{
+                $sql = $sql . " AND $key=$value";
+            }
+            $i++;
+        }
+    }
+
+    $query = $conn->prepare($sql);
+    $query->execute();
+    
+    return $query->fetch();
+
+}
+
+
 function update($table, $id, $param = []){
 
     global $conn;
